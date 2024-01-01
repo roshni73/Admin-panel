af14e9d548639c2620d0faaaa13f5a4fb83b8ea1
roshni poudel <99161691+roshni73@users.noreply.github.com>
2024-01-01 21:14:26 +0545

    Config updated

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [react(),  tsconfigPaths()],
 
   server: {
